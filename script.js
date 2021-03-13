7b4ea83ed737daba1b064a69ad5ce298f90efa0b
Corey <57112820+corey613@users.noreply.github.com>
2021-03-13 16:34:06 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+   wrapAround: true,
+   autoPlay: true
+});
